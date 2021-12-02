6bdd1f4ef08ed4a6ab8c703797e4ff87f0a867af
pikolinian <pikolinian@gazeta.pl>
2021-12-02 11:26:48 +0100

    day2 finished

--- src/adv/day2.clj
+++ src/adv/day2.clj
@@ -1,1 +1,45 @@
-(ns adv.day2)
+(ns adv.day2
+   (:require [clojure.string :as str]))
+
+(defn adjust-position
+  "use position and depth. command is [:com value] 
+   :forward first +
+    :down second +
+    :up second -
+   "
+  [old-pos [command value]]
+  (cond
+    (= :forward command) [(+ (first old-pos) value) (last old-pos)]
+    (= :up command) [(first old-pos) (- (last old-pos) value)]
+    (= :down command) [(first old-pos) (+ (last old-pos) value)]
+    ))
+
+(defn adjust-aim-pos
+  ""
+  [[old-aim old-x old-depth] [command value]]
+   (cond
+     (= :forward command) [old-aim (+ old-x value) (+ old-depth (* old-aim value))]
+     (= :up command) [(- old-aim value) old-x old-depth]
+     (= :down command) [(+ old-aim value) old-x old-depth])
+  )
+
+(defn day-2-p-1 [comms]
+  (let [dist-pair (reduce adjust-position [0 0] comms)]
+    (* (first dist-pair) (last dist-pair))))
+
+(defn day-2-p-2 [comms]
+  (let [dist-pair (reduce adjust-aim-pos [0 0 0] comms)]
+    (* (second dist-pair) (last dist-pair))
+    ))
+
+(defn tokenify
+  "convert string to vector [:direction value]"
+  [string]
+  (let [pair (str/split string #" ")]
+    [(keyword (first pair)) (Integer/parseInt (last pair))]))
+
+(defn get-answer [path-to-file]
+  (let [s (map tokenify (str/split-lines (slurp path-to-file)))]
+   (time (println "Answer for first part is: " (day-2-p-1 s)))
+    (time (println "Answer for second part is: " (day-2-p-2 s)))
+    ))
--- test/adv/day2_test.clj
+++ test/adv/day2_test.clj
@@ -1,3 +1,26 @@
 (ns adv.day2-test
   (:require [clojure.test :refer [deftest is testing]]
-            [adv.day2 :as sut]))
+            [adv.day2 :as sut]
+            [clojure.string :as str]))
+
+(def test-input (map sut/tokenify (str/split-lines "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2")))
+
+(deftest tokenify-test
+  (testing "tokenify f-ction"
+   (is (= [:forward 5] (sut/tokenify "forward 5")))
+    (is (= [:down 5] (sut/tokenify "down 5")))))
+
+(deftest adjust-test
+  (testing "adjust"
+   (is (= [5 0] (sut/adjust-position [0 0] [:forward 5])))))
+
+(deftest part-1
+  (testing "part-1"
+    (is(= 15 (sut/day-2-p-1 [[:forward 5] [:down 4] [:up 1]])))
+    (is(= 150 (sut/day-2-p-1 test-input)))    
+    ))
+
+(deftest part-2
+  (testing "part-2"
+    (is (= 900 (sut/day-2-p-2 test-input)))
+    ))
