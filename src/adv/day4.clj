1611fa1e9b55453ff4465117898ef7c91446fb47
Bzyk <42726269+pikolinianita@users.noreply.github.com>
2021-12-04 23:59:11 +0100

    Update day4.clj

--- src/adv/day4.clj
+++ src/adv/day4.clj
@@ -1,14 +1,31 @@
 (ns adv.day4
   (:require [clojure.string :as str]))
 
+(def bingo-size 5)
+
 (defn make-board [lines]
   (let [numerify (fn [str] (Integer/parseInt str))
         line-to-vector (fn [line] (vec (map numerify (re-seq #"\d+" line))))] 
-    (vec (map line-to-vector lines))))
+     (vec (apply concat [] (map line-to-vector lines)))))
 
 (defn hit [board n]
-  
-  )
+	(let [hit-pos (.indexOf board n)]
+		(if (pos? hit-pos) 
+		(update-in board [hit-pos] #(+ 100 %))
+		board)
+	))
+
+(defn solved? [board]
+(let [size (int (Math/sqrt (count board)))
+	test-row (fn [pos] (->> board (drop (* pos size)) (take size)))
+	test-col (fn [pos] (->> board (drop pos) (take-nth size)))
+	hit? (fn [line] (every? #(< 100 %) line))]
+
+ (true? (or
+	(some true? (map hit? (map test-row (range size))))
+	(some true? (map hit? (map test-col (range size))))))
+
+))
 
 
 (defn day-4-p-1 [s]
