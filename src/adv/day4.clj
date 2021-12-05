805e24779f2646dc0ec23bc346c23de502fbd7f1
Bzyk <42726269+pikolinianita@users.noreply.github.com>
2021-12-05 13:18:02 +0100

    Update day4.clj

--- src/adv/day4.clj
+++ src/adv/day4.clj
@@ -3,14 +3,19 @@
 
 (def bingo-size 5)
 
+(defn print-square [board]	
+	(doall (map println (partition 5 board)))
+	(println)
+)
+
 (defn make-board [lines]
   (let [numerify (fn [str] (Integer/parseInt str))
         line-to-vector (fn [line] (vec (map numerify (re-seq #"\d+" line))))] 
      (vec (apply concat [] (map line-to-vector lines)))))
 
 (defn hit [board n]
-	(let [hit-pos (.indexOf board n)]
-		(if (pos? hit-pos) 
+	(let [hit-pos (.indexOf board n)]	
+		(if (<= 0 hit-pos) 
 		(update-in board [hit-pos] #(+ 100 %))
 		board)
 	))
@@ -27,9 +32,33 @@
 
 ))
 
+(defn final-score-p1 [board n]
+
+(println board n)
+n
+)
+
+(defn try-solve-p1 [shots boards]
+(let [hit-boards (vec (map #(hit % (first shots)) boards))
+	  solved-vec (map solved? hit-boards)]
+	;(println (first shots) (map solved? hit-boards))
+	;(doall (map print-square hit-boards))
+	;(println (class hit-boards))
+	(if (every? false? solved-vec)
+		(recur (rest shots) hit-boards)
+		(final-score-p1 (get hit-boards (.indexOf solved-vec true)) (first shots)) 
+		;(str "finished " (first shots))
+)
+))
+
 
 (defn day-4-p-1 [s]
-  "not implemented")
+	(let [shots (map #(Integer/parseInt %) (str/split (first s) #","))
+		board-str (map make-board (filter #(< 1 (count %)) (partition-by #(= "" %) (rest s))))]
+	
+(try-solve-p1 shots board-str)
+
+  ))
 
 (defn day-4-p-2 [s]
   "not implemented")
